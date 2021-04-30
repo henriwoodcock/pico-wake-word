2d58eef8cf8125ab56e22952f25fda46c652469b
Sandeep Mistry <sandeep.mistry@arm.com>
2021-04-30 09:13:52 -0400

    Change audio provider to be more like Arduino one

--- micro_speech/rp2/audio_provider.cc
+++ micro_speech/rp2/audio_provider.cc
@@ -1,42 +1,22 @@
-#include "audio_provider.h"
-#include "micro_features/micro_model_settings.h"
-
-#include <string.h>
 #include "pico/stdlib.h"
 #include "hardware/adc.h"
 #include "hardware/dma.h"
 #include "hardware/irq.h"
 
-#include <limits>
+#include "audio_provider.h"
 
-// set this to determine sample rate
-// 0     = 500,000 Hz
-// 960   = 50,000 Hz
-// 9600  = 5,000 Hz
-// -> 3000 = 16,000 Hz
-#define CAPTURE_FREQUENCY 16000
-// Channel 0 is GPIO26
-#define ADC_PIN 26
-#define CAPTURE_CHANNEL 0
-#define LED_PIN 25
-
-// BE CAREFUL: anything over about 9000 here will cause things
-// to silently break. The code will compile and upload, but due
-// to memory issues nothing will work properly
-#define NSAMP 256 // similar to arduino example
-#define BUFFER_SIZE 16000
+//#include "PDM.h"
+#include "micro_features/micro_model_settings.h"
 
 namespace {
-// dma settings
-dma_channel_config cfg;
-uint dma_chan;
 
-uint16_t g_audio_sample_buffer[2][NSAMP];
-// tflite micro settings
+#define ADC_BUFFER_SIZE 256
+#define ADC_PIN 26
+#define CAPTURE_CHANNEL 0
+
 bool g_is_audio_initialized = false;
-volatile int capture_index = 0;
 // An internal buffer able to fit 16x our sample size
-constexpr int kAudioCaptureBufferSize = NSAMP * 16 * 2;
+constexpr int kAudioCaptureBufferSize = ADC_BUFFER_SIZE * 16;
 int16_t g_audio_capture_buffer[kAudioCaptureBufferSize];
 // A buffer that holds our output
 int16_t g_audio_output_buffer[kMaxAudioSampleSize];
@@ -44,11 +24,13 @@ int16_t g_audio_output_buffer[kMaxAudioSampleSize];
 // any samples have arrived yet.
 volatile int32_t g_latest_audio_timestamp = 0;
 
-} // namespace
+dma_channel_config cfg;
+uint dma_chan;
+uint16_t g_audio_sample_buffer[2][ADC_BUFFER_SIZE];
+volatile int capture_index = 0;
+}  // namespace
 
-//this next function is the dma interupt
 void CaptureSamples() {
-  //reset the interrupt request first so you do not lose an interupt
   // Clear the interrupt request.
   dma_hw->ints0 = 1u << dma_chan;
   // get the current capture index
@@ -56,25 +38,36 @@ void CaptureSamples() {
   // get the next capture index to send the dma to start
   capture_index = (capture_index + 1) % 2;
   // Give the channel a new wave table entry to read from, and re-trigger it
-  dma_channel_transfer_to_buffer_now(dma_chan,
-    g_audio_sample_buffer[capture_index], NSAMP);
-  // data processing
-  const int number_of_samples = NSAMP;
+  dma_channel_transfer_to_buffer_now(dma_chan, g_audio_sample_buffer[capture_index], ADC_BUFFER_SIZE);
+
+
+  // This is how many bytes of new data we have each time this is called
+  const int number_of_samples = ADC_BUFFER_SIZE;
   // Calculate what timestamp the last audio sample represents
-  const int32_t time_in_ms = g_latest_audio_timestamp + (number_of_samples / (kAudioSampleFrequency / 1000));
+  const int32_t time_in_ms =
+      g_latest_audio_timestamp +
+      (number_of_samples / (kAudioSampleFrequency / 1000));
   // Determine the index, in the history of all samples, of the last sample
-  const int32_t start_sample_offset = g_latest_audio_timestamp * (kAudioSampleFrequency / 1000);
+  const int32_t start_sample_offset =
+      g_latest_audio_timestamp * (kAudioSampleFrequency / 1000);
   // Determine the index of this sample in our ring buffer
-  const int array_capture_index = start_sample_offset % kAudioCaptureBufferSize;
+  const int capture_index = start_sample_offset % kAudioCaptureBufferSize;
   // Read the data to the correct place in our buffer
-  memcpy(g_audio_capture_buffer + array_capture_index,
-    (void *)g_audio_sample_buffer[read_index],
-    sizeof(int16_t)*number_of_samples);
-  // when this changes the nn runs
+  // PDM.read(g_audio_capture_buffer + capture_index, DEFAULT_PDM_BUFFER_SIZE);
+  int16_t* out = g_audio_capture_buffer + capture_index;
+  uint16_t* in = g_audio_sample_buffer[read_index];
+
+  for (int i = 0; i < ADC_BUFFER_SIZE; i++) {
+    *out++ = (*in++ - /*(1.25 * 4095 / 3.3 )*/ 1551);
+  }
+
+  // This is how we let the outside world know that new audio data has arrived.
   g_latest_audio_timestamp = time_in_ms;
 }
 
-void setup() {
+TfLiteStatus InitAudioRecording(tflite::ErrorReporter* error_reporter) {
+  // Hook up the callback that will be called with each sample
+
   adc_gpio_init(ADC_PIN + CAPTURE_CHANNEL);
 
   adc_init();
@@ -88,7 +81,7 @@ void setup() {
 		 );
 
   // set sample rate
-  adc_set_clkdiv((48000000 / CAPTURE_FREQUENCY) - 1);
+  adc_set_clkdiv((48000000 / kAudioSampleFrequency) - 1);
 
   sleep_ms(1000);
   // Set up the DMA to start transferring data as soon as it appears in FIFO
@@ -106,7 +99,7 @@ void setup() {
   dma_channel_configure(dma_chan, &cfg,
 			NULL,    // dst
 			&adc_hw->fifo,  // src
-			NSAMP,          // transfer count
+			ADC_BUFFER_SIZE,          // transfer count
 			false            // start immediately
 	);
 
@@ -118,17 +111,14 @@ void setup() {
 
   adc_run(true); //start running the adc
 
-  dma_channel_transfer_to_buffer_now(dma_chan, g_audio_sample_buffer[capture_index],
-    NSAMP);
-}
+  dma_channel_transfer_to_buffer_now(dma_chan, g_audio_sample_buffer[capture_index], ADC_BUFFER_SIZE);
 
-TfLiteStatus InitAudioRecording(tflite::ErrorReporter* error_reporter) {
-  // Hook up the callback that will be called with each sample
-  setup();
-  // Manually call the handler once, to trigger the first transfer
-  CaptureSamples();
-  // let first samples roll in
-  sleep_ms(1000);
+
+  // PDM.onReceive(CaptureSamples);
+  // // Start listening for audio: MONO @ 16KHz with gain at 20
+  // PDM.begin(1, kAudioSampleFrequency);
+  // PDM.setGain(20);
+
   // Block until we have our first audio sample
   while (!g_latest_audio_timestamp) {
   }
@@ -154,35 +144,20 @@ TfLiteStatus GetAudioSamples(tflite::ErrorReporter* error_reporter,
   // often enough and the buffer is large enough that this call will be made
   // before that happens.
 
-  const int16_t kAdcSampleDC = 0;
-  const int16_t kAdcSampleGain = 1;
-
   // Determine the index, in the history of all samples, of the first
   // sample we want
-  //printf("getting audio samples\n");
   const int start_offset = start_ms * (kAudioSampleFrequency / 1000);
   // Determine how many samples we want in total
-  const int duration_sample_count = duration_ms * (kAudioSampleFrequency / 1000);
+  const int duration_sample_count =
+      duration_ms * (kAudioSampleFrequency / 1000);
   for (int i = 0; i < duration_sample_count; ++i) {
     // For each sample, transform its index in the history of all samples into
     // its index in g_audio_capture_buffer
-    const int array_capture_index = (start_offset + i) % kAudioCaptureBufferSize;
-    const int32_t capture_value = g_audio_capture_buffer[array_capture_index];
-    int32_t output_value = capture_value - 0x7ff;
-    //
-    output_value *= kAdcSampleGain;
-    //printf("%d, \n", output_value);
-    if (output_value < std::numeric_limits<int16_t>::min()) {
-      output_value = std::numeric_limits<int16_t>::min();
-    }
-    if (output_value > std::numeric_limits<int16_t>::max()) {
-      output_value = std::numeric_limits<int16_t>::max();
-    }
+    const int capture_index = (start_offset + i) % kAudioCaptureBufferSize;
     // Write the sample to the output buffer
-    g_audio_output_buffer[i] = output_value;
-    //printf("%d\n", output_value);
-    //g_audio_output_buffer[i] = capture_value;
+    g_audio_output_buffer[i] = g_audio_capture_buffer[capture_index];
   }
+
   // Set pointers to provide access to the audio
   *audio_samples_size = kMaxAudioSampleSize;
   *audio_samples = g_audio_output_buffer;
