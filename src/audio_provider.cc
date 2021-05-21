9732f386b50a6b3dfa373b216d45e80b21122058
Henri Woodcock <henri.woodcock@arm.com>
2021-05-21 15:49:17 +0100

    initial update

---
+++ src/audio_provider.cc
@@ -0,0 +1,179 @@
+/* Copyright 2018 The TensorFlow Authors. All Rights Reserved.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+#include "audio_provider.h"
+#include "micro_features/micro_model_settings.h"
+
+#include "pico/stdlib.h"
+#include "hardware/adc.h"
+#include "hardware/dma.h"
+#include "hardware/irq.h"
+
+#define ADC_PIN 26
+#define CAPTURE_CHANNEL 0
+#define ADC_BIAS ((int16_t)((1.25 * 4095) / 3.3))
+#define ADC_BUFFER_SIZE 256
+
+namespace {
+
+bool g_is_audio_initialized = false;
+// An internal buffer able to fit 16x our sample size
+constexpr int kAudioCaptureBufferSize = ADC_BUFFER_SIZE * 16;
+int16_t g_audio_capture_buffer[kAudioCaptureBufferSize];
+// A buffer that holds our output
+int16_t g_audio_output_buffer[kMaxAudioSampleSize];
+// Mark as volatile so we can check in a while loop to see if
+// any samples have arrived yet.
+volatile int32_t g_latest_audio_timestamp = 0;
+
+dma_channel_config cfg;
+uint dma_chan;
+uint16_t g_audio_sample_buffer[2][ADC_BUFFER_SIZE];
+volatile int dma_capture_index = 0;
+
+} // namespace
+
+void CaptureSamples() {
+  // Clear the interrupt request.
+  dma_hw->ints0 = 1u << dma_chan;
+  // get the current capture index
+  int read_index = dma_capture_index;
+  // get the next capture index to send the dma to start
+  dma_capture_index = (dma_capture_index + 1) % 2;
+  // Give the channel a new wave table entry to read from, and re-trigger it
+  dma_channel_transfer_to_buffer_now(dma_chan, g_audio_sample_buffer[dma_capture_index], ADC_BUFFER_SIZE);
+
+
+  // This is how many bytes of new data we have each time this is called
+  const int number_of_samples = ADC_BUFFER_SIZE;
+  // Calculate what timestamp the last audio sample represents
+  const int32_t time_in_ms =
+      g_latest_audio_timestamp +
+      (number_of_samples / (kAudioSampleFrequency / 1000));
+  // Determine the index, in the history of all samples, of the last sample
+  const int32_t start_sample_offset =
+      g_latest_audio_timestamp * (kAudioSampleFrequency / 1000);
+  // Determine the index of this sample in our ring buffer
+  const int capture_index = start_sample_offset % kAudioCaptureBufferSize;
+  // Read the data to the correct place in our buffer
+  // PDM.read(g_audio_capture_buffer + capture_index, DEFAULT_PDM_BUFFER_SIZE);
+  int16_t* out = g_audio_capture_buffer + capture_index;
+  uint16_t* in = g_audio_sample_buffer[read_index];
+
+  for (int i = 0; i < ADC_BUFFER_SIZE; i++) {
+    *out++ = (*in++ - ADC_BIAS);
+  }
+
+  // This is how we let the outside world know that new audio data has arrived.
+  g_latest_audio_timestamp = time_in_ms;
+}
+
+TfLiteStatus InitAudioRecording(tflite::ErrorReporter* error_reporter) {
+  // Hook up the callback that will be called with each sample
+
+  adc_gpio_init(ADC_PIN + CAPTURE_CHANNEL);
+
+  adc_init();
+  adc_select_input(CAPTURE_CHANNEL);
+  adc_fifo_setup(
+		 true,    // Write each completed conversion to the sample FIFO
+		 true,    // Enable DMA data request (DREQ)
+		 1,       // DREQ (and IRQ) asserted when at least 1 sample present
+		 false,   // We won't see the ERR bit because of 8 bit reads; disable.
+		 false     // Shift each sample to 8 bits when pushing to FIFO
+		 );
+
+  // set sample rate
+  adc_set_clkdiv((48000000 / kAudioSampleFrequency) - 1);
+
+  sleep_ms(1000);
+  // Set up the DMA to start transferring data as soon as it appears in FIFO
+  uint dma_chan = dma_claim_unused_channel(true);
+  cfg = dma_channel_get_default_config(dma_chan);
+
+  // Reading from constant address, writing to incrementing byte addresses
+  channel_config_set_transfer_data_size(&cfg, DMA_SIZE_16);
+  channel_config_set_read_increment(&cfg, false);
+  channel_config_set_write_increment(&cfg, true);
+
+  // Pace transfers based on availability of ADC samples
+  channel_config_set_dreq(&cfg, DREQ_ADC);
+
+  dma_channel_configure(dma_chan, &cfg,
+			NULL,    // dst
+			&adc_hw->fifo,  // src
+			ADC_BUFFER_SIZE,          // transfer count
+			false            // start immediately
+	);
+
+  // Tell the DMA to raise IRQ line 0 when the channel finishes a block
+  dma_channel_set_irq0_enabled(dma_chan, true);
+  // Configure the processor to run dma_handler() when DMA IRQ 0 is asserted
+  irq_set_exclusive_handler(DMA_IRQ_0, CaptureSamples);
+  irq_set_enabled(DMA_IRQ_0, true);
+
+  adc_run(true); //start running the adc
+
+  dma_channel_transfer_to_buffer_now(dma_chan, g_audio_sample_buffer[dma_capture_index], ADC_BUFFER_SIZE);
+
+
+  // PDM.onReceive(CaptureSamples);
+  // // Start listening for audio: MONO @ 16KHz with gain at 20
+  // PDM.begin(1, kAudioSampleFrequency);
+  // PDM.setGain(20);
+
+  // Block until we have our first audio sample
+  while (!g_latest_audio_timestamp) {
+  }
+
+  return kTfLiteOk;
+}
+
+TfLiteStatus GetAudioSamples(tflite::ErrorReporter* error_reporter,
+                             int start_ms, int duration_ms,
+                             int* audio_samples_size, int16_t** audio_samples) {
+  // Set everything up to start receiving audio
+  if (!g_is_audio_initialized) {
+    TfLiteStatus init_status = InitAudioRecording(error_reporter);
+    if (init_status != kTfLiteOk) {
+      return init_status;
+    }
+    g_is_audio_initialized = true;
+  }
+  // This next part should only be called when the main thread notices that the
+  // latest audio sample data timestamp has changed, so that there's new data
+  // in the capture ring buffer. The ring buffer will eventually wrap around and
+  // overwrite the data, but the assumption is that the main thread is checking
+  // often enough and the buffer is large enough that this call will be made
+  // before that happens.
+
+  // Determine the index, in the history of all samples, of the first
+  // sample we want
+  const int start_offset = start_ms * (kAudioSampleFrequency / 1000);
+  // Determine how many samples we want in total
+  const int duration_sample_count =
+      duration_ms * (kAudioSampleFrequency / 1000);
+  for (int i = 0; i < duration_sample_count; ++i) {
+    // For each sample, transform its index in the history of all samples into
+    // its index in g_audio_capture_buffer
+    const int capture_index = (start_offset + i) % kAudioCaptureBufferSize;
+    // Write the sample to the output buffer
+    g_audio_output_buffer[i] = g_audio_capture_buffer[capture_index];
+  }
+
+  // Set pointers to provide access to the audio
+  *audio_samples_size = kMaxAudioSampleSize;
+  *audio_samples = g_audio_output_buffer;
+
+  return kTfLiteOk;
+}
+
+int32_t LatestAudioTimestamp() { return g_latest_audio_timestamp; }
